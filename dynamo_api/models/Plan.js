42cffe5fa8e34f841b18327513bf820be24008d9
Antonhy Retto Lopez - EVERIS <aretto@bcp.com.pe>
2019-08-27 11:57:46 -0500

    plans

---
+++ dynamo_api/models/Plan.js
@@ -0,0 +1,31 @@
+'use strict';
+var dynamo = require('dynamodb');
+const Joi = require('joi');
+
+//dynamo.AWS.config.loadFromPath('credentials.json');
+var dynamo = require('dynamodb');
+
+dynamo.AWS.config.update({
+  accessKeyId: require("../../../backend_bicibici_admin/config").accessKeyId, 
+  secretAccessKey: require("../../../backend_bicibici_admin/config").secretAccessKey, 
+  region: require("../../../backend_bicibici_admin/config").Region});
+  
+var Plan = dynamo.define('Plan', {
+    hashKey : 'PlanID',
+   
+    // add the timestamp attributes (updatedAt, createdAt)
+    timestamps : true,
+   
+    schema : {
+      PlanID : Joi.number(),
+      Cost   : Joi.number(),
+      Duration    : Joi.number()
+    }
+  });
+
+  
+  Station.config({tableName: 'Plan'});
+
+  module.exports = dynamo.model("Plan",Plan);
+
+  
